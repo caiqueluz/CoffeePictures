ebac2acdd8d0016a2a94d015885d7218c6442aac
caiqueluz <eucaiqueluz@gmail.com>
2025-11-23 12:05:11 -0300

    feat: add basic view model abstraction

---
+++ app/src/main/java/com/example/coffeepictures/BasicViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.coffeepictures
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+abstract class BasicViewModel<ViewStateT> : ViewModel() {
+    private val mutableViewState by lazy {
+        MutableStateFlow(
+            value = createViewState(),
+        )
+    }
+
+    val viewState by lazy {
+        mutableViewState.asStateFlow()
+    }
+
+    protected fun updateViewState() {
+        mutableViewState.update {
+            createViewState()
+        }
+    }
+
+    protected abstract fun createViewState(): ViewStateT
+}
