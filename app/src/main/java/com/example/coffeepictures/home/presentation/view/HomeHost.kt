202e8605866520a789ba0c8db3ed9b6cb2469f33
caiqueluz <eucaiqueluz@gmail.com>
2025-11-23 13:10:05 -0300

    fix: fix home not loading the first time it appears

--- app/src/main/java/com/example/coffeepictures/home/presentation/view/HomeHost.kt
+++ app/src/main/java/com/example/coffeepictures/home/presentation/view/HomeHost.kt
@@ -3,6 +3,7 @@ package com.example.coffeepictures.home.presentation.view
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onFirstVisible
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.coffeepictures.home.presentation.logic.HomeViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -15,7 +16,7 @@ fun HomeHost(
     val viewState by viewModel.viewState.collectAsStateWithLifecycle()
 
     HomeView(
-        modifier = modifier,
+        modifier = modifier.onFirstVisible(callback = viewModel::onHomeStarted),
         viewState = viewState,
         onLoadNewButtonClicked = viewModel::onLoadNewButtonClicked,
         onAddToFavoritesButtonClicked = viewModel::onAddToFavoritesButtonClicked,
