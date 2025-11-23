7712f994b26024290432d2d9fe21f792dddb56fb
caiqueluz <eucaiqueluz@gmail.com>
2025-11-23 18:50:11 -0300

    feat: add app toolbar host

--- app/src/main/java/com/example/coffeepictures/app/presentation/AppHost.kt
+++ app/src/main/java/com/example/coffeepictures/app/presentation/AppHost.kt
@@ -1,19 +1,34 @@
 package com.example.coffeepictures.app.presentation
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.example.coffeepictures.apptoolbar.presentation.view.AppToolbarHost
 import com.example.coffeepictures.home.presentation.view.HomeHost
+import kotlinx.coroutines.flow.MutableStateFlow
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppHost(
     modifier: Modifier = Modifier,
     snackbarHostState: SnackbarHostState,
 ) {
+    val appScreenFlow =
+        remember {
+            MutableStateFlow<AppScreenModel>(value = AppScreenModel.Home)
+        }
+
     Scaffold(
         modifier = modifier,
+        topBar = {
+            AppToolbarHost(
+                appScreenFlow = appScreenFlow,
+            )
+        },
         snackbarHost = {
             SnackbarHost(
                 hostState = snackbarHostState,
---
+++ app/src/main/java/com/example/coffeepictures/apptoolbar/presentation/view/AppToolbarHost.kt
@@ -0,0 +1,30 @@
+package com.example.coffeepictures.apptoolbar.presentation.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.coffeepictures.app.presentation.AppScreenModel
+import com.example.coffeepictures.apptoolbar.presentation.logic.AppToolbarViewModel
+import kotlinx.coroutines.flow.StateFlow
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
+
+@Composable
+fun AppToolbarHost(
+    modifier: Modifier = Modifier,
+    appScreenFlow: StateFlow<AppScreenModel>,
+) {
+    val viewModel =
+        koinViewModel<AppToolbarViewModel> {
+            parametersOf(appScreenFlow)
+        }
+
+    val viewState by viewModel.viewState.collectAsStateWithLifecycle()
+
+    AppToolbarView(
+        modifier = modifier,
+        viewState = viewState,
+        onToolbarIconClicked = viewModel::onToolbarActionIconClicked,
+    )
+}
