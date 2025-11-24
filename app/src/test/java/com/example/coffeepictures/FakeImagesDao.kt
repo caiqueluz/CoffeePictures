6c575093feb857d889d7a7ea6f4ac96470a172f7
caiqueluz <eucaiqueluz@gmail.com>
2025-11-23 22:34:03 -0300

    test: add FakeImagesDao

---
+++ app/src/test/java/com/example/coffeepictures/FakeImagesDao.kt
@@ -0,0 +1,25 @@
+package com.example.coffeepictures
+
+import com.example.coffeepictures.database.database.ImageEntity
+import com.example.coffeepictures.database.database.ImagesDao
+
+class FakeImagesDao : ImagesDao {
+    private val entities = mutableListOf<ImageEntity>()
+    private var hasFakeGetAllError = false
+
+    override suspend fun insert(entity: ImageEntity) {
+        entities.add(entity)
+    }
+
+    override suspend fun getAllImages(): List<ImageEntity> {
+        if (hasFakeGetAllError) {
+            throw Exception("Get all images fake error.")
+        }
+
+        return entities
+    }
+
+    fun fakeGetAllImagesError() {
+        hasFakeGetAllError = true
+    }
+}
